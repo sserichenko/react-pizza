0345f661740e6470b6477fe5db1074333689b64e
Сергій Сериченко <Sergey.Serichenko@systemgroup.com.ua>
2021-11-01 18:44:34 +0200

    half of lesson 5

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ import "./scss/app.scss";
 
 function App() {
 
+  const [pizzas, setPizzas] = React.useState([]);
+
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/db.json")
+    .then(response => response.json())
+    .then(json => setPizzas(json.pizzas))
+  }, [])
+
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home pizzas={pizzas}/>} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
